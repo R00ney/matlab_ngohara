a5d7c1f52ca3e43073039026a0dc3c1bd11d95da
R00ney <ngo1991@gmail.com>
2013-09-28 23:08:47 -0400

    new example

---
+++ 4hw/DC Motor Example/motor_equations_script.m
@@ -0,0 +1,14 @@
+%motor_equations constants script
+
+% moment of inertia of rotor
+J = 0.01; %kg*m^2
+% motor viscous friction constant 
+b = 0.1; % N.m.s
+% electromotive force constant       
+Ke =   0.01; % V/rad/sec
+%   motor torque constant             
+Kt =  0.01; % N.m/Amp
+%    electric resistance                
+R = 1; %Ohm
+%    electric inductance              
+L =   0.5; %H
